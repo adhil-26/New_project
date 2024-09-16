0a12d06341fd37b932f658c44348cfa2f6beb6f6
Adhil <adhilboss23@gmail.com>
2024-09-16 10:07:09 -0400

    Message

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ["Pancakes", "Eggs Benedict", "Oatmeal", "Frittata"];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ["Pancakes- $12", "Eggs Benedict- $22.99", "Oatmeal- $21.99", "Frittata- $15"];
+const mainCourseMenu = ['Steak- $100', 'Pasta- $75.99', 'Burger- %29.99', 'Salmon- $49.99'];
+const dessertMenu = ['Cake- $18.99', 'Ice Cream- $12.99', 'Pudding- $16.99', 'Fruit Salad- $9.99'];
 
 // Generate HTML for breakfast menu items
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
